f50765c364e9ce9fcd0a0f96e382655e9f65f020
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2025-08-26 14:56:27 +0530

    Seed roles

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -7,6 +8,35 @@ namespace NZWalks.API.Data
     {
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "8e235086-2c23-4208-95e3-50d3591c625d";
+            var writerRoleId = "da2efd8d-6a24-49ee-bdaf-ed0eb913184e";
+
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
