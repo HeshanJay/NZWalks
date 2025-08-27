0488578ac96bb141c2eccb78e153e2542a3da023
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2025-08-27 14:55:35 +0530

    Implement image upload repository

--- NZWalks.API/Controllers/ImagesController.cs
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
 
 namespace NZWalks.API.Controllers
 {
@@ -7,6 +10,13 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
         // POST: /api/Images/Upload
         [HttpPost]
         [Route("Upload")]
@@ -16,8 +26,21 @@ namespace NZWalks.API.Controllers
 
             if (ModelState.IsValid)
             {
-                // User repository to upload image
+                // Covert DTO to domain model
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileSizeInBytes = request.File.Length,
+                    FileName = Path.GetFileNameWithoutExtension(request.File.FileName),
+                    FileDescription = request.FileDescription
+                };
 
+
+                // User repository to upload image
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
             }
 
             return BadRequest(ModelState);
@@ -33,7 +56,7 @@ namespace NZWalks.API.Controllers
             }
             if (request.File.Length > 10485760)
             {
-                ModelState.AddModelError("file", "File size more than 10MB, please upload a smaller size file.")
+                ModelState.AddModelError("file", "File size more than 10MB, please upload a smaller size file.");
             }
         }
     }
