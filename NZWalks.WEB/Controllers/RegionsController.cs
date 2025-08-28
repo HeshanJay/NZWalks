b6e81695f2eb017fbc02d4bc1bb3d6796b9a23f1
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2025-08-28 11:32:26 +0530

    Implement get all regions - consume web api

---
+++ NZWalks.WEB/Controllers/RegionsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.WEB.Models.DTO;
+
+namespace NZWalks.WEB.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+
+            try
+            {
+                // Get all regions form web API
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7117/api/regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+            }
+
+            return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
